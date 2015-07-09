796afb7fa9129730fd3e93c170ce0d88c430b398
Ben Voynick <ben.voynick@gmail.com>
2015-07-08 22:14:26 -0400

    Use minification on index.html (editable source now resides in html-src subdirectory)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 
 var imageminJpegtran = require('imagemin-jpegtran');
+var minifyHTML = require('gulp-minify-html');
+var minifyInline = require('gulp-minify-inline');
 
 gulp.task('jpegs', function () {
 	return gulp.src('img-src/*.jpg')
@@ -10,4 +12,11 @@ gulp.task('jpegs', function () {
 
 gulp.task('images', ['jpegs']);
 
-gulp.task('default', ['images']);
+gulp.task('minify-html', function () {
+	return gulp.src('html-src/*.html')
+		.pipe(minifyHTML({conditionals: true}))
+		.pipe(minifyInline())
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['images', 'minify-html']);
