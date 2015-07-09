1d2adf32a3d421376940e45b854ae5181343564d
Ben Voynick <ben.voynick@gmail.com>
2015-07-08 22:44:39 -0400

    Add git-rename and changed HTML minifying process to rename *.src.html files to *.html so that relative paths are correct
    This was needed to use git-inline to inline CSS files on demand. (currently inlining style.css only.)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 
+var rename = require('gulp-rename');
 var imageminJpegtran = require('imagemin-jpegtran');
+var inlineSource = require('gulp-inline-source');
 var minifyHTML = require('gulp-minify-html');
 var minifyInline = require('gulp-minify-inline');
 
@@ -13,9 +15,14 @@ gulp.task('jpegs', function () {
 gulp.task('images', ['jpegs']);
 
 gulp.task('minify-html', function () {
-	return gulp.src('html-src/*.html')
+	return gulp.src('./*.src.html')
+		.pipe(inlineSource())
 		.pipe(minifyHTML({conditionals: true}))
 		.pipe(minifyInline())
+		.pipe(rename(function(path) {
+			path.basename = path.basename.replace('.src', '');
+			//return path;
+		}))
 		.pipe(gulp.dest('./'));
 });
 
