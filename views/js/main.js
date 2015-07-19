d4d5317351ccfe90349be3c9e9c0ce9cae5225d9
Ben Voynick <ben.voynick@gmail.com>
2015-07-19 19:28:10 -0400

    Refactor changePizzaSizes to use getElementByClassName and move variable calculation outside for loop

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,13 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randomPizzaContainers = document.getElementsByClassName("randomPizzaContainer");
+    var randomPizzaContainersLength = randomPizzaContainers.length;
+    
+    for (var i = 0; i < randomPizzaContainersLength; i++) {
+      var dx = determineDx(randomPizzaContainers[i], size);
+      var newwidth = (randomPizzaContainers[i].offsetWidth + dx) + 'px';
+      randomPizzaContainers[i].style.width = newwidth;
     }
   }
 
