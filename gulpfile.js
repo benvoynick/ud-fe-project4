55f17bdad5a4d6a650cf1e00b53cff4039313660
Ben Voynick <ben.voynick@gmail.com>
2015-07-19 20:15:41 -0400

    Move development and production files into 'src' and 'prod' directories
    Change gulp workflow accordingly
    
    Disable use of imagemin for now because it is throwing an error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,40 @@
 var gulp = require('gulp');
 
-var rename = require('gulp-rename');
 var imageminJpegtran = require('imagemin-jpegtran');
 var inlineSource = require('gulp-inline-source');
 var minifyHTML = require('gulp-minify-html');
 var minifyInline = require('gulp-minify-inline');
-
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+ 
 gulp.task('jpegs', function () {
-	return gulp.src('img-src/*.jpg')
-		.pipe(imageminJpegtran({})())
-		.pipe(gulp.dest('img'));
+	return gulp.src('./src/**/*.jpg')
+		//.pipe(imageminJpegtran({})())
+		.pipe(gulp.dest('./prod/'));
 });
 
 gulp.task('images', ['jpegs']);
 
+gulp.task('minify-css', function () {
+	return gulp.src('./src/**/*.css')
+		.pipe(minifyCss({compatibility: '*'}))
+		.pipe(gulp.dest('./prod/'));
+});
+
+gulp.task('uglify-js', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./prod/'));
+});
+
 gulp.task('minify-html', function () {
-	return gulp.src('./*.src.html')
+	return gulp.src('./src/**/*.html')
 		.pipe(inlineSource())
 		.pipe(minifyHTML({conditionals: true}))
 		.pipe(minifyInline())
-		.pipe(rename(function(path) {
-			path.basename = path.basename.replace('.src', '');
-			//return path;
-		}))
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest('./prod/'));
 });
 
-gulp.task('default', ['images', 'minify-html']);
+gulp.task('minify', ['minify-css', 'uglify-js', 'minify-html']);
+
+gulp.task('default', ['images', 'minify']);
